2a7addf24edccd06692b509c671a6dfb9582fdcb
oonishi_daijiro <oonishi-daijiro@users.noreply.github.com>
2023-12-11 18:33:05 +0900

    Chore: Remove api directory.This is because we cannot use API route on static nextjs website :(

--- src/pages/api/get-all-products.ts
+++
@@ -1,8 +0,0 @@
-import stock from "./stock.json";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req:NextApiRequest,res:NextApiResponse){
-   console.log('get all products');
-   
-   res.status(200).json(stock)
-}
--- src/pages/api/order.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import stock from "./stock.json";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  stock.stock[0].stock--;
-  res.status(200).send("");
-}
