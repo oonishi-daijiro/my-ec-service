3884ba423e87ec1efffe7e1f98d341ebb8b751cc
OonishiDaijiro <72515127+oonishi-daijiro@users.noreply.github.com>
2024-12-25 14:46:37 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === "production" ? "my-ec-service" : ""
+  basePath: process.env.NODE_ENV === "production" ? "/my-ec-service" : ""
 }
 
 module.exports = nextConfig
